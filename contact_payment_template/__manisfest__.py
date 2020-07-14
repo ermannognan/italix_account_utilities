9ada2e10c1ef2e0a07ee38832f9997cda8ac6a3a
gleydsonhudson <ghudson.ti@gmail.com>
2020-07-15 01:35:53 +0200

    [WIP] dev on module contract_payment_template

---
+++ contact_payment_template/__manisfest__.py
@@ -0,0 +1,21 @@
+# Copyright 2015 Nicola Malcontenti - Agile Business Group
+# Copyright 2016 Andrea Cometa - Apulia Software
+# Copyright 2016-2019 Lorenzo Battistini
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Account Payment Template",
+    'summary': 'Create a payment template for proforma',
+    'version': '12.0.1.0.0',
+    'category': 'Account',
+    'author': 'Ermanno Gnan, Gleydson Silva',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+    ],
+    "data": [
+        
+    ],
+    "installable": True
+}
